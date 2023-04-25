41e87548a01a8b9d5cd7f233d0e63c255e25a42a
xFa00 <fchirang@unal.edu.co>
2023-04-24 20:37:14 -0500

    Ejercicio 19

--- src/ejercicio_19/Ejercicio_19.java
+++ src/ejercicio_19/Ejercicio_19.java
@@ -10,7 +10,7 @@ public class Ejercicio_19 {
    
     public static void main(String[] args) {
      VentanaPrincipal ventana= new VentanaPrincipal(); //Constructor ventana
-     ventana.setVisible(true); //Metodo para hacer visible la ventana
+     ventana.setVisible(true);//Metodo para hacer visible la ventana
     }
     
 }
