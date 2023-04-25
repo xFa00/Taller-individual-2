e3d996c609f5086e8ce98a108e96161038e02eae
xFa00 <fchirang@unal.edu.co>
2023-04-25 11:25:05 -0500

    Ejercicio 19

--- src/ejercicio_18/Ventana2.java
+++ src/ejercicio_18/Ventana2.java
@@ -134,7 +134,7 @@ public class Ventana2 extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-               new Ventana2().setVisible(true);
+              new Ventana2().setVisible(true);
             }
         });
     }
