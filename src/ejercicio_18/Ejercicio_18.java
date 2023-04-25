46ede0ebf4b4f5f6732b05878438681dcec9c73a
xFa00 <fchirang@unal.edu.co>
2023-04-24 20:30:18 -0500

    Ejercicio 18

---
+++ src/ejercicio_18/Ejercicio_18.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_18;
+
+
+public class Ejercicio_18 {
+
+
+    public static void main(String[] args) {
+     VentanaP ventana= new VentanaP(); //Constructor ventana
+     ventana.setVisible(true);//Metodo para hacer visible la ventana
+    }
+    
+}
