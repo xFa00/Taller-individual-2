88da304f5ea60af62e268e286e5b0b71db5b5d4a
xFa00 <fchirang@unal.edu.co>
2023-04-29 23:47:07 -0500

    Ejercicios

---
+++ src/ejercicio_22/CalculoSalario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_22;
+
+
+public class CalculoSalario {
+    public static int calculo(int horasT, int salarioH){
+        int salarioT;
+        salarioT = horasT*salarioH;
+        return salarioT;
+        
+    }
+    
+}
---
+++ src/ejercicio_22/Ejercicio_22.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_22;
+
+
+public class Ejercicio_22 {
+
+    
+    public static void main(String[] args) {
+       VentanaPrincipal ventana= new VentanaPrincipal(); //Constructor ventana
+       ventana.setVisible(true);//Metodo para hacer visible la ventanaa
+    }   
+}
