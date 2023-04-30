1eb273a4cd92805d0cc6e56e9801d5301a4a270e
xFa00 <fchirang@unal.edu.co>
2023-04-29 23:49:58 -0500

    Ejercicios

---
+++ src/ejercicio/pkg19/Ejercicio19.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg19;
+
+/**
+ *
+ * @author ACER
+ */
+public class Ejercicio19 {
+
+   
+    public static void main(String[] args) {
+     VentanaPrincipal ventana= new VentanaPrincipal(); //Constructor ventana
+     ventana.setVisible(true);//Metodo para hacer visible la ventana
+    }
+    
+}
+
