6c2e8ae79f2801d8aa7568f9277c89230a009039
xFa00 <fchirang@unal.edu.co>
2023-05-19 22:20:17 -0500

    Ejercicios

---
+++ src/ejercicio/pkg18/SalarioNeto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.pkg18;
+
+public class SalarioNeto {
+    public static double CalcularSalarioNeto(double salarioB, double retencion){
+       double salarioN;
+       retencion= salarioB*(retencion/100);
+       salarioN = salarioB-retencion;
+       return salarioN;
+    }
+}
