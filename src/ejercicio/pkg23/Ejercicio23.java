1e8b2bf8c6307eae1e30f716d590a31fc170332c
xFa00 <fchirang@unal.edu.co>
2023-05-19 22:16:44 -0500

    Ejercicios

---
+++ src/ejercicio/pkg23/Ejercicio23.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.pkg23;
+
+/**
+ *
+ * @author ACER
+ */
+public class Ejercicio23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      VentanaPrincipal ventana= new VentanaPrincipal(); //Constructor ventana
+      ventana.setVisible(true);//Metodo para hacer visible la ventanaa
+    }
+    
+}
